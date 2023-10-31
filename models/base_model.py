c216eb7dce6bedb13f10562f30273f91398948a2
romeollangozi <romeollangozi123@gmail.com>
2023-10-31 17:36:27 +0100

    Reshaped init function to handle args and kwargs arguments

--- models/base_model.py
+++ models/base_model.py
@@ -24,9 +24,12 @@ class BaseModel:
             for key, value in kwargs.items():
                 if key != '__class__':
                     setattr(self, key, value)
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+                    if key == 'created_at' or key == "updated_at":
+                        setattr(self, key, datetime.fromisoformat(value))
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         '''

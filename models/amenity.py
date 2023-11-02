d60c01d102ebe31d3904e9c1e98c7db62fccd81e
SilviMarku <marku.silvia777@gmail.com>
2023-11-02 12:13:59 +0000

    Amenity

--- models/amenity.py
+++ models/amenity.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python3
+"""
+This defines an Amenity class that inherits from BaseModel.
+"""
 
 
 from models.base_model import BaseModel
 
 class Amenity(BaseModel):
+    """
+    A class variable for 'name' with an initial value of an empty string
+    """
+    name + ""
+
     def __init__(self, *args, **kwargs):
+        """
+        Initialize a Amenity instance, that inherits initialization from BaseModel.
+        """
         super().__init__(*args, **kwargs)
-        self.name = ""
+    
---
+++ models/user.py
@@ -0,0 +1,11 @@
+from models.base_model import BaseModel
+
+class User(BaseModel):
+    """User class that inherits from BaseModel"""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.email = ""
+        self.password = ""
+        self.first_name = ""
+        self.last_name = ""

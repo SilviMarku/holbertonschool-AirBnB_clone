1b1f332ddcb3747fa710556860c8ac8c4d6f46b2
SilviMarku <marku.silvia777@gmail.com>
2023-11-02 12:37:48 +0000

    Reviwe

--- models/review.py
+++ models/review.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
-
+"""
+This defines a Review class that inherits from BaseModel.
+"""
 
 from models.base_model import BaseModel
 
+
 class Review(BaseModel):
-    def __init__(self, *args, **kwargs):
+        place_id = ""
+        user_id = ""
+        text = ""
+
+        def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.place_id = ""
-        self.user_id = ""
-        self.text = ""
         

8a2cd6afaac670c9b408a6ced059cd77e3cddbc4
romeollangozi <romeollangozi123@gmail.com>
2023-11-02 20:14:02 +0100

    s

--- models/__init__.py
+++ models/__init__.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""
+'''
 This script initializes a FileStorage instance and reloads data,
 making it available for managing and persisting data.
-"""
+'''
 
 
 from models.engine.file_storage import FileStorage
